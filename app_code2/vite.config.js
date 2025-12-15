7196dc2f8e8563b7fa6fb61254c600a23bc2d6a3
tianyong <tianyong0618>
2025-12-15 17:54:35 +0800

    适配cloudbase

--- app_code2/vite.config.js
+++ app_code2/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   server: {
     port: 5522,
     host: '0.0.0.0'

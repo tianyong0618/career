c221dda737e0c6735516146128676f7e3eeaef8c
tianyong <tianyong0618>
2025-12-16 09:15:55 +0800

    适配cloudbase

--- web_code2/vite.config.js
+++ web_code2/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // 确保构建后的资源路径正确
+  base: './', // 使用相对路径，确保部署后资源能正确加载
   server: {
     host: '0.0.0.0', // 支持IP访问
     port: 5521 // 指定端口为5521

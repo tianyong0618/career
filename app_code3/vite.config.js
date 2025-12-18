afcfe8c159bcb22f7ff0b4bfdc482165e66e026b
tianyong <tianyong0618>
2025-12-18 09:14:11 +0800

    适配cloudbase

--- app_code3/vite.config.js
+++ app_code3/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // 设置相对路径，解决cloudbase部署白屏问题
   server: {
     host: '0.0.0.0', // 支持IP访问
     port: 5532 // 指定端口为5532
